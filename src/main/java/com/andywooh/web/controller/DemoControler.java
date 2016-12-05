20de8b556038df380097e1e3b90fa4af7b9b1c1c
andywooh <andywooh@sina.com>
2016-12-05 15:34:46 +0800

    test master

--- src/main/java/com/andywooh/web/controller/DemoControler.java
+++ src/main/java/com/andywooh/web/controller/DemoControler.java
@@ -15,7 +15,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+/**
+ * this is master
+ * @author wuuuxjia
+ *
+ */
 @Controller
 @RequestMapping(value = "demo")
 public class DemoControler {
